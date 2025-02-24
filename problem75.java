33ebdf7aa2694a9a39447c0ec2c60acfbbe5edfe
Samuel Santos <samuel.santos@dcx.ufpb.br>
2025-02-24 10:24:07 -0300

    Update problem75.java

--- problem75.java
+++ problem75.java
@@ -10,6 +10,7 @@ class problem75 {
         System.out.println(Arrays.toString(nums));
     }
 
+    //Bubble sort.
     public static void sortColors(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length - i - 1; j++) {
